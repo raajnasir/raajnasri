b8b3dc4dfb82b4f7a8fba297a660e170e064b3a5
raajnasir <70377100+raajnasir@users.noreply.github.com>
2020-09-13 17:28:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,33 @@
-var car,wall;
+var bullet,wall,thickness;
 var speed,weight;
 
 function setup() {
   createCanvas(1600,400);
   
-speed = random(55,90);
-weight = random(400,1500);
-car = createSprite(50,200,50,50);
-car.velocityX = speed;
-wall = createSprite(750,200,60,height/2)
+speed = random(223,321);
+weight = random(30,52);
+thickness = random(22,83)
+bullet = createSprite(50,200,50,50);
+bullet.velocityX = speed;
+wall = createSprite(1200,200,thickness,height/2)
 }
 
 function draw() {
   background(255,255,255);  
- if(wall.x-car.x<(car.width+wall.width)/2)
+ if(hasCollided(bullet,wall))
  {
-   car.velocity = 0;
-   var deformation = 0.5 * weight * speed * speed/22509;
-        if(deformation>180)
-        {
-           car.shapeColor = color(255,0,0);
-     if(deformation<180 &&deformation>100)
+     bullet.velocityX = 0;
+     var damage = 0.5 *weight * speed * speed/(thickness * thickness);
+
+     if(damage>10)
      {
-       car.shapeColor = color(230,230,0);
+         wall.shapeColor =color(255,0,0);
      }
-          if(deformation<100)
-          {
-            car.shapeColor = color(0,255,0)
-          }
-  }
+
+     if (damamge<10)
+     {
+         wall.shapeColor = color(0,250,0);
+    }
  }
    drawSprites();
 }
